2420a529e2cdcf05a9b3744f492fd0c5b3ada49e
Doveccl <doveccl@live.com>
2024-04-05 04:18:43 +0800

    final fix for mac build

--- fix.mjs
+++
@@ -1,7 +0,0 @@
-import { stat, symlink } from 'node:fs/promises'
-
-if (process.platform === 'darwin') {
-  // fix *.dylib not found on macOS
-  const path = 'node_modules/@u4/opencv-build/opencv'
-  await stat(path).catch(() => symlink('dist/latest', path))
-}
